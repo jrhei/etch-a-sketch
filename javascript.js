8a7bcabc2b0a7ee649671a0eab98593e1b26f4b4
jrhei <jannorheydon@gmail.com>
2022-03-25 16:39:26 +0800

    BETA 0.03-Reset button and change grid size * Add reset button which resets the colored grid back to white * Add reset functionality which prompts the user to enter a size of the next grid to be created * Add createGrid which creates a grid based on the users input from reset button * Add removeGrid function which removes current grid * Change color changing procedure to a function named changeColor for the boxes inside the grid

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 const container = document.querySelector('#drawingArea')
-
+const xDiv = document.createElement('div');
+const yDiv = document.createElement('div');
 for (i = 1; i <= 16; i++){
     const xDiv = document.createElement('div');
     xDiv.classList.add('xblock');
@@ -12,8 +13,78 @@ for (i = 1; i <= 16; i++){
         xDiv.appendChild(yDiv);
     }
 }
+function createGrid(num = 16){
+    for (i = 1; i <= num; i++){
+        const xDiv = document.createElement('div');
+        xDiv.classList.add('xblock');
+        xDiv.textContent = "";
+        container.appendChild(xDiv);
+        for (x = 1 ;x <= num; x++){
+            const yDiv = document.createElement('div');
+            yDiv.classList.add('yblock');
+            yDiv.textContent="";
+            xDiv.appendChild(yDiv);
+        }
+    }
+    const yblock = document.querySelectorAll('.yblock'); //queries the grids
+
+    yblock.forEach(yblock => yblock.addEventListener('mouseover', function(e){ //when hovering over the blocks they change a specific color
+        console.log(e.target);
+        //yblock.style.cssText ='background: red'; 
+        changeColor(yblock);
+}));
+}
+
 const yblock = document.querySelectorAll('.yblock'); //queries the grids
+
 yblock.forEach(yblock => yblock.addEventListener('mouseover', function(e){ //when hovering over the blocks they change a specific color
     console.log(e.target);
-    yblock.style.cssText ='background: red'; 
+    //yblock.style.cssText ='background: red'; 
+    changeColor(yblock);
 }));
+
+const reset = document.querySelector('#reset');
+reset.addEventListener('click', function(e){
+    yblock.forEach(resetColor);
+    console.log(e.target);
+    const removeVarx = document.querySelectorAll('.xblock');
+    removeVarx.forEach(removeGrid);
+    const removeVary = document.querySelectorAll('.yblock');
+    removeVary.forEach(removeGrid);
+    let size = prompt("Enter Grid size: ");
+    createGrid(size);
+    
+
+});
+function resetColor(block){
+    block.style.cssText='background: white';
+}
+function changeColor(block){
+    block.style.cssText='background: red';
+}
+function removeGrid(block){
+    block.remove();
+}
+/*
+Reference code for mousedown and mouse
+
+let state = false;
+const yblock = document.querySelectorAll('.yblock'); //queries the grids
+yblock.forEach(yblock => yblock.addEventListener('mousedown', function(e){
+    state = true;
+}));
+
+yblock.forEach(yblock => yblock.addEventListener('mouseup', function(e){
+    state = false;
+}));
+
+yblock.forEach(yblock => yblock.addEventListener('mouseover', function(e){ //when hovering over the blocks they change a specific color
+    console.log(e.target);
+    //yblock.style.cssText ='background: red'; 
+    if (state === true){
+        changeColor(yblock);
+    }
+    else if(state === false){
+        return;
+    }
+}));*/
